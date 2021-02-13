fddd164d30d2329360b4d6990b41ef6c4d30faac
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2021-02-13 23:05:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const ejs = require("ejs");
+
+const app = express();
+const tasksToDo = [];
+let randomIdx = Math.floor(Math.random()*100);
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+
+
+app.get("/",function(req, res){
+	const url = "https://type.fit/api/quotes";
+	let quote;
+
+	https.get(url, function (response) {
+    	const chunks = []
+	    response.on('data', function (chunk) {
+	      chunks.push(chunk)
+	    })
+
+	    response.on('end', function () {
+	      const data = Buffer.concat(chunks)
+	      var quoteData = JSON.parse(data)
+	      quote = quoteData[randomIdx].text;
+	      console.log(quote);
+	      res.render("tasks", {displayQuote : quote, tasksLeft : tasksToDo});
+	    })
+    })
+});
+
+app.post("/", function(req, res){
+	let task = {
+		title : req.body.title,
+		description : req.body.description
+	}; 
+	tasksToDo.push(task);
+	res.redirect("/");
+});
+
+app.listen(3000, function(){
+	console.log("Server etd. at port 3000");
+});
---
+++ public/index.js
@@ -0,0 +1,93 @@
+let added = true;
+
+//Intro Title Display
+const date = new Date();
+if(date.getHours() >= 12 && date.getHours() < 17){
+	$("h1").text("Good Afternoon");
+}
+
+else if(date.getHours() >= 17){
+	$("h1").text("Good Evening");
+	$("h5").text("Welcome Back!");
+}
+
+//Day and date display
+let options = {
+	weekday : 'long',
+	day : 'numeric',
+	month : 'long'
+};
+
+function checkHeight(){
+	if($(".task-card").length < 7 && !added){
+		$("div.image-div").addClass("set-height");
+		added = true
+	}
+	else if($(window).width() > 1028 && $(".task-card").length > 6 && added){
+		$("div.image-div").removeClass("set-height");
+		added = false;
+	}
+	if($(window).width() < 770)
+		$("div.content-container").prepend("<br>");
+}
+
+
+function doneTasksPercentage(){
+	let doneTasks = $(".my-tasks .green").length;
+    let totalTasks = $(".task-card").length;	//total tasks
+    let percentDone = Math.floor((doneTasks*100)/totalTasks);
+    let width = "width :"+percentDone+"%";
+    $("div.progress-bar").attr("style", width);
+}
+
+
+$("textarea.form-control").on('keyup paste', function () {
+	let maxchars = 30;
+    $(this).val($(this).val().substring(0, maxchars));
+    let tlength = $(this).val().length;
+    let remain = parseInt(tlength);
+    $(".input-task p").text(remain+"/30");
+});
+
+
+const day = date.toLocaleDateString("en-US",options);
+$("p.day-date").text(day);
+
+//blur event
+$(".add-task-btn").on("click", function(){
+	$("div.image-div").addClass("blur-bg");
+	$("div.content-container").addClass("blur-bg");
+	$("div.make-progress").addClass("blur-bg");
+	$("div.my-tasks").addClass("blur-bg");
+	$("div.input-task").addClass("elementToFadeIn");	
+
+	// Cancelling the task 
+	$("button.cancel-btn").on("click", function(){
+		$("div.input-task").removeClass("elementToFadeIn");
+		$("div.make-progress").removeClass("blur-bg");
+		$("div.my-tasks").removeClass("blur-bg");
+		$("div.image-div").removeClass("blur-bg");
+		$("div.content-container").removeClass("blur-bg");
+	});
+});
+
+$("button.done").on("click", function(){
+	let containerDiv = $(this).parent();
+	let descriptionDiv = containerDiv.parent();
+    descriptionDiv.addClass("green");
+    containerDiv.remove();
+    doneTasksPercentage();
+});
+
+$("button.trash").on("click", function(){
+	let buttonDiv = $(this).parent();
+	let descriptionDiv = buttonDiv.parent();
+	let cardDiv = descriptionDiv.parent();
+	let finDiv = cardDiv.parent();
+	finDiv.remove();
+	$("div.image-div").css("background-size","cover");
+	checkHeight();
+	doneTasksPercentage();
+});
+
+checkHeight();
